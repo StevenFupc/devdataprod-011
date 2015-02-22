ae5eb74bac24b3e65a26289d17ef09fca5661b01
StevenFupc <fupengchen@gmail.com>
2015-02-23 04:20:48 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,16 +3,16 @@ shinyUI(
     pageWithSidebar(
         headerPanel("How much is the diamond?"),
         sidebarPanel(
-            h3("Enter desired weight of diamond, and get an estimated price"),
-            h3("Please enter a diamond weight in carats (0.15 - 0.35)"),
+            h4("Enter desired weight of diamond, and get an estimated price and range, using linear regression."),
+            h4("Please enter a diamond weight in carats (0.15 - 0.35)"),
             numericInput('weight',"Diamond weight (in carat)",value=0.20,min=0.15,max=0.35,step=0.01),
-            h3("You have entered:"),
+            h4("You have entered:"),
             verbatimTextOutput("inputValue"),
-            h3("Price of your desired diamond is likely to be (in SGD):"),
+            h4("Price of your desired diamond is likely to be (in SGD):"),
             verbatimTextOutput("price"),
-            h3("in the range of"),
+            h4("in the range of"),
             verbatimTextOutput("lower"), 
-            h3("and"),
+            h4("and"),
             verbatimTextOutput("higher")  
         ),
         mainPanel(
